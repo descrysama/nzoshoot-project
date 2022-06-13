40eee32cb6f29aa84c1998e8e64d5bf2ba0732f0
DescrySama <erwing2002@gmail.com>
2022-06-13 16:02:59 +0200

    add photos

--- front/src/pages/admin/albums/AdminAddPhoto.jsx
+++ front/src/pages/admin/albums/AdminAddPhoto.jsx
@@ -19,22 +19,35 @@ const AdminAddPhoto = () => {
         });
     }, [])
 
+    const RemoveHandler = (albumid) => {
+        
+    }
+
     return(
         <>
             <AdminNavbar/>
-            <div className="d-flex flex-direction-column justify-content-center align-items-center container animate__animated animate__fadeIn p-2">
-                {loading ?
-                    <i className="fa-solid fa-spinner fa-2xl white-icon animate__animated animate__infinite animate__rotateOut m-2"></i>
-                :
-                <>
-                     {images.map((item, key) => (
-                        <div key={key} className="d-flex flex-direction-column justify-content-center align-items-center col-lg" style={{margin: "10px", textAlign: 'center'}}>
-                            <img src={`${process.env.REACT_APP_IMAGE}/`+ item.image_path} alt={item.description} width="80%"/>
-                            <p className="album-info m-2">{item.description}</p>
-                        </div>
-                    ))}
+            <div className="animate__animated animate__fadeIn p-2">
+                <div className="d-flex align-items-center flex-direction-column" style={{justifyContent: 'space-around'}}>
+                    <input type="file" className="yellowbutton" multiple/><i ></i>
                     <Link to="/ns-nimda/albums"><i className="fa-solid fa-arrow-left-long white-icon fa-lg"></i> Retour</Link>
-                </>}
+                </div>
+                <div className="d-flex justify-content-center align-items-center container animate__animated animate__fadeIn p-2">
+                
+                    {loading ?
+                        <i className="fa-solid fa-spinner fa-2xl white-icon animate__animated animate__infinite animate__rotateOut m-2"></i>
+                    :
+                    <>
+                        {images.map((item, key) => (
+                            <div key={key} className="d-flex flex-direction-column justify-content-center align-items-center album-card col-lg" style={{margin: "10px", textAlign: 'center', width: '8em'}}>
+                                <img src={`${process.env.REACT_APP_IMAGE}/`+ item.image_path} alt={item.description} width="80%"/>
+                                <p className="album-info" style={{margin: "2px"}}>{item.description}</p>
+                                <li onClick={() => RemoveHandler(item.id)} className="yellowbutton"><i className="fa-solid fa-trash-can"></i></li>
+                            </div>
+                        ))}
+                        
+                    </>}
+                    
+                </div>
             </div>
         </>
     )
